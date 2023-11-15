80b9f6bee653a43ec246825e074bbd80d20a234d
Jordan J <jordanr2m@gmail.com>
2023-11-14 17:09:11 -0800

    Added functionality to allow buttons to be clicked multiple times.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const shareIcon1 = document.querySelector(".share-icon1");
+const shareIcon2 = document.querySelector(".share-icon2");
 const authorSection = document.querySelector(".author");
 const shareSectionMobile = document.querySelector(".share-container-mobile");
 const shareSectionDesktop = document.querySelector(".share-container-desktop");
@@ -7,10 +8,14 @@ const shareSectionDesktop = document.querySelector(".share-container-desktop");
 shareIcon1.addEventListener("click", () => {
     // Desktop code
     if (window.matchMedia("(min-width: 768px)").matches) {
-        shareSectionDesktop.classList.remove("hidden");
+        shareSectionDesktop.classList.toggle("hidden");
     // Mobile code
     } else {
         authorSection.classList.add("hidden");
         shareSectionMobile.classList.remove("hidden");
+        shareIcon2.addEventListener("click", () => {
+            authorSection.classList.remove("hidden");
+            shareSectionMobile.classList.add("hidden");
+        });
     }
 });
